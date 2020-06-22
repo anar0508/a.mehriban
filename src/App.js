2c241bfa538eec11d252eb897cf1f1e81d3b4c05
Anar0508 <anar_ismayilov@ymail.com>
2020-06-22 07:00:59 -0700

    Transfer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,70 @@
 import React from 'react';
-import styled from 'styled-components';
+import '../index.css';
+import Login from './Login/Login';
+import Homepage from './Components/Homepage/Homepage';
+import RegistrationContainer from './Registration/RegistrationContainer';
+import CabinetContainer from './Cabinet/CabinetContainer';
+import CoinsListPageContainer from './CoinsList/CoinsListPageContainer';
+import CoinContainer from './Coin/CoinContainer';
+import CartContainer from './Cart/CartContainer';
+import { Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div>
-      <header>
-        Hello there!
-      </header>
-    </div>
-  );
+const Main = styled.main`
+    width: 100%,
+    @media screen and (min-width: 600px) {
+    display: flex 
+}  
+`;
+
+function App(props) {
+    return (
+        <Main>
+            <nav>
+                <ul>
+
+                </ul>
+            </nav>
+            <Router>
+                <Switch>
+                    <Route path="/" exact>
+                        <Homepage/>
+                    </Route>
+                    <Route path="/login" exact>
+                        <Login />
+                    </Route>
+                    <Route path="/cabinet" exact>
+                        <CabinetContainer />
+                    </Route>
+                    <Route path="/photo/:id" exact>
+                        <CoinsListPageContainer />
+                    </Route>
+                    <Route path="/set/:type" exact>
+                        <CoinContainer />
+                    </Route>
+                    <Route path="/about" exact>
+                        <CartContainer />
+                    </Route>
+                    <Route path="/contact" exact>
+                        <CartContainer />
+                    </Route>
+                </Switch>
+            </Router>
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </Main>
+    );
 }
+
 
 export default App;
